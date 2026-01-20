332e996718461005743ee7f31ca7a3091501c7ae
Lise TUONG <lise.tuong@ipsa.fr>
2026-01-20 13:43:37 +0100

    Projet Rover Sprint 3

--- Navigation/main.py
+++ Navigation/main.py
@@ -3,34 +3,66 @@ from IPSA_ROVER_Lib import IpsaRoverLib
 
 driver = IpsaRoverLib()
 
-def avance_cm(distance_cm):
+def linear_move_cm(distance_cm):
     """
-    distance_cm positive = déplacement vers l'avant
+    distance_cm > 0 : déplacement en avant
     """
-    temps = distance_cm / 19.4
-    if distance_cm > 0:
-        driver.control_motor_speed(-200,-200,-200,-200)
+    ticks_target = int(distance_cm * 124)
+
+    start_ticks = driver.read_total_encoder_counts()
+    start = start_ticks[0]  # on prend un moteur de référence
+
+    driver.control_motor_speed(-200, -200, -200, -200)
+
+    while True:
+        current = driver.read_total_encoder_counts()[0]
+        if abs(current - start) >= ticks_target:
+            break
+        time.sleep(0.01)
+
+    driver.control_motor_speed(0, 0, 0, 0)
+
+def lateral_move_cm(distance_cm):
+    """
+    distance_cm > 0 : déplacement à droite
+    """
+    ticks_target = int(distance_cm * 124)
+    start = driver.read_total_encoder_counts()[0]
+
+    driver.control_motor_speed(200, -200, -200, 200)
+
+    while True:
+        current = driver.read_total_encoder_counts()[0]
+        if abs(current - start) >= ticks_target:
+            break
+        time.sleep(0.01)
+
+    driver.control_motor_speed(0, 0, 0, 0)
+
+def turn_degree(angle):
+    """
+    angle > 0 : tourne à droite
+    angle < 0 : tourne à gauche
+    """
+    arc_cm = abs(angle) * 3.1416 * 17.1 / 360
+    ticks_target = int(arc_cm * 248)
+
+    start = driver.read_total_encoder_counts()[0]
+
+    if angle > 0:
+        driver.control_motor_speed(200, 200, -200, -200)
     else:
-        driver.control_motor_speed(200,200,200,200)
-    time.sleep(temps)
-    driver.control_motor_speed(0,0,0,0)
+        driver.control_motor_speed(-200, -200, 200, 200)
 
-def cote_cm(distance_cm):
-    """
-    distance_cm positive = déplacement sur la droite
-    """
-    temps = distance_cm / 19.4
-    if distance_cm > 0:
-        driver.control_motor_speed(200,-200,-200,200)
-    else:
-        driver.control_motor_speed(-200,200,200,-200)
-    time.sleep(temps)
-    driver.control_motor_speed(0,0,0,0)
+    while True:
+        current = driver.read_total_encoder_counts()[0]
+        if abs(current - start) >= ticks_target:
+            break
+        time.sleep(0.01)
 
-def tourne_degre(angle):
-    driver.control_motor_speed(200,200,-200,-200)
-    time.sleep(3)
-    driver.control_motor_speed(0,0,0,0)
+    driver.control_motor_speed(0, 0, 0, 0)
 
-avance_cm(20)
-cote_cm(20)
+
+linear_move_cm(20)
+lateral_move_cm(20)
+turn_degree(90)
